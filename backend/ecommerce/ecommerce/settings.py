cd572a980850841b338afd285a00251fc26bfad3
pratyush3060 <tpratyush0@gmail.com>
2026-05-26 09:01:58 +0530

    Add STATICFILES_STORAGE for legacy django-cloudinary-storage compatibility

--- backend/ecommerce/ecommerce/settings.py
+++ backend/ecommerce/ecommerce/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # Static files
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media files
 MEDIA_URL = "/media/"
